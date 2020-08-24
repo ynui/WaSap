19808bbc7933e880c5bd9ec954757b1287f8e007
Yuval Niezni <ynui12@gmail.com>
2020-08-24 22:13:21 +0300

    v2

--- index.js
+++
@@ -1,7 +0,0 @@
-function start() {
-    let pNumber = document.getElementById("pNumber").value;
-    let message = document.getElementById("message").value;
-    pNumber = pNumber.replace(/\D/g,'');
-    let URL = "https://wa.me/" + pNumber + "?text=" + encodeURI(message);
-    window.open(URL);
-}
